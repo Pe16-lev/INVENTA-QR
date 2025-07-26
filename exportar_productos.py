fcd1a5a94d5a1e512315efeb826cd7e9403deaf7
Pe16-lev <p8841141@gmail.com>
2025-07-26 15:27:10 -0500

    cambio nombre de carpeta

--- exportar_productos.py
+++ exportar_productos.py
@@ -3,7 +3,7 @@ import json
 import os
 
 DB_NAME = 'inventario.db'
-JSON_PATH = os.path.join('codigo', 'productos.json')  # Cambiado a 'telaraña'
+JSON_PATH = os.path.join('codigo', 'productos.json')  # Cambiado a codigo
 
 campos = [
     "Numero Serial", "Tipo de equipo", "Marca", "Modelo", "Cantidad",
